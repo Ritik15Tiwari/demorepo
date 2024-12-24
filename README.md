a2e32acbb9801968108efb717192aae432f67d4f
Ritik15Tiwari <ritiktiwari15012002@gmail.com>
2024-12-25 00:00:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# My new GITHUB project
+This is my new GITHUB project that i published directly from visual studio code
---
+++ new.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class new{
+    public static void main(String[] args) {
+        int a= 2;
+        int b=6;
+        int avg= (a + b)/2;
+        System.out.println(avg);
+    }
+}
